d1e9b6abee61c0ba36b2975157ab05f8f9f5c22c
SimonZhong <Zhong_1109267330@outlook.com>
2020-07-27 23:04:28 +0800

    Create 02 蟒蛇实例.py

---
+++ Simon‘s base camp/Python/MOOC python语言程序设计/02 蟒蛇实例.py
@@ -0,0 +1,17 @@
+#PythonDraw.py 蟒蛇绘制图形是一切图形绘制的基础
+import turtle
+turtle.setup(650, 350, 200, 200)
+turtle.penup()
+turtle.fd(-250)
+turtle.pendown()
+turtle.pensize(25)
+turtle.pencolor("purple")
+turtle.seth(-40)
+for i in range(4)
+    turtle.circle(40, 80)
+    turtle.circle(-40, 80)
+turtle.circle(40, 80/2)
+turtle.fd(40)
+turtle.circle(16, 180)
+turtle.fd(40 * 2/3)
+turtle.done()
