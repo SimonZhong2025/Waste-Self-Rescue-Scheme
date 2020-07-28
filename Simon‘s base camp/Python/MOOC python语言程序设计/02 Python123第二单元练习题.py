e3811ee5e14e8234cf3c717a7a323362153e7ec9
SimonZhong <Zhong_1109267330@outlook.com>
2020-07-28 15:58:40 +0800

    Update 02 Python123第二单元练习题.py
    增加了turtle风轮绘制

--- Simon‘s base camp/Python/MOOC python语言程序设计/02 Python123第二单元练习题.py
+++ Simon‘s base camp/Python/MOOC python语言程序设计/02 Python123第二单元练习题.py
@@ -5,3 +5,23 @@ for i in range(8):
     t.fd(100)
     t.left(45)
     
+# 风车绘制，运用到goto函数
+import turtle as t
+t.pensize(2)
+for i in range(4):
+    t.fd(150)
+    t.right(90)
+    t.circle(-150, 45)
+    t.goto(0,0)
+    t.left(45)
+
+#也可以这样
+#WindWheel.py
+import turtle as t
+t.pensize(2)
+for i in range(4):
+    t.seth(90*i)
+    t.fd(150)
+    t.right(90)
+    t.circle(-150, 45)
+    t.goto(0,0)
