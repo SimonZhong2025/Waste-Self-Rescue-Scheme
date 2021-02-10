03b586baa451b171d90c618ffa8cf49b74b706e8
Simon <Zhong_1109267330@outlook.com>
2021-02-10 22:12:11 +0800

    Update 05 七段数码管.py

--- Simon‘s base camp/Python/MOOC python语言程序设计/编程文件/05 七段数码管.py
+++ Simon‘s base camp/Python/MOOC python语言程序设计/编程文件/05 七段数码管.py
@@ -5,54 +5,55 @@ Created on Tue Feb  9 12:34:15 2021
 @author: Zhong
 """
 
-import turtle, time
+import turtle as t
+import time
 
 def drawLine(draw):
-    turtle.penup()
-    turtle.fd(10)
-    turtle.pendown() if draw else turtle.penup()
-    turtle.fd(40)
-    turtle.penup()
-    turtle.fd(10)
-    turtle.right(90)
+    t.penup()
+    t.fd(10)
+    t.pendown() if draw else turtle.penup()
+    t.fd(40)
+    t.penup()
+    t.fd(10)
+    t.right(90)
     
 def drawDigit(digit):
     drawLine(True) if digit in [2,3,4,5,6,8,9] else drawLine(False)
     drawLine(True) if digit in [0,1,3,4,5,6,7,8,9] else drawLine(False)
     drawLine(True) if digit in [0,2,3,5,6,8,9] else drawLine(False)
     drawLine(True) if digit in [0,2,6,8] else drawLine(False)
-    turtle.left(90)
+    t.left(90)
     drawLine(True) if digit in [0,4,5,6,8,9] else drawLine(False)
     drawLine(True) if digit in [0,2,3,5,6,7,8,9] else drawLine(False)
     drawLine(True) if digit in [0,1,2,3,4,7,8,9] else drawLine(False)
-    turtle.left(180)
-    turtle.penup()
-    turtle.fd(20)
+    t.left(180)
+    t.penup()
+    t.fd(20)
     
 def drawDate(date):
-    turtle.pencolor("red")
+    t.pencolor("red")
     for i in date:
         if  i == '-':
-            turtle.pencolor("green")
-            turtle.write("年",font=("Arial", 18, "normal"))
-            turtle.fd(40)
+            t.pencolor("green")
+            t.write("年",font=("Arial", 18, "normal"))
+            t.fd(40)
         elif i == '=':
-            turtle.write("月",font=("Arial", 18, "normal"))
-            turtle.fd(40)
+            t.write("月",font=("Arial", 18, "normal"))
+            t.fd(40)
         elif i == '+':            
-            turtle.write("日",font=("Arial", 18, "normal"))
-            turtle.fd(40)
+            t.write("日",font=("Arial", 18, "normal"))
+            t.fd(40)
         else:
             drawDigit(eval(i))
         
 def main():
-    turtle.setup(800,350,200,200)
-    turtle.penup()
-    turtle.fd(-300)
-    turtle.pensize(5)
+    t.setup(800,350,200,200)
+    t.penup()
+    t.fd(-300)
+    t.pensize(5)
     drawDate(time.strftime("%Y-%m=%d+",time.gmtime()))
-    turtle.hihdeturtle()
-    turtle.done()
+    t.hihdeturtle()
+    t.done()
     
 main()
 
